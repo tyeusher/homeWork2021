2b2988de8564aa7b9de079821b7c97eeb4f38e6a
tyeusher <86023106+tyeusher@users.noreply.github.com>
2021-08-03 15:36:09 -0500

    Add files via upload

---
+++ .vscode/classActivities/week5HomeWork/week5Constructor.js
@@ -0,0 +1,25 @@
+
+class generalCategory{
+    constructor(main, types, year){
+        this.main = main;
+        this.type = type;
+        this.year = year
+    }
+}
+
+
+class generalSport extends generalCategory{
+    constructor(location, department){
+        super(main, types);
+        this.location = location;
+        this.department = department;
+    }
+}
+
+class generalAdmission extends generalCategory{
+    constructor(contact, level){
+super(main, type);
+this.contact = contact;
+this.level = level;
+    }
+}
